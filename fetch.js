d9928722daf85b5a7703200e340154235876bace
tanim-k <tanimkhan877@gmail.com>
2022-03-05 14:36:26 +0600

    asynchronous examples fetch

--- fetch.js
+++ fetch.js
@@ -11,4 +11,8 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
 .then(response => response.json())
 .then(json => console.log(json))
 console.log(7777777)
-console.log(1111111)
+console.log(1111111);
+for (let i = 0; i < 40; i++) {
+    console.log(i);
+    
+}
