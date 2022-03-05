29e178aa57803eba85aa6fda5461ba2da5819b98
tanim-k <tanimkhan877@gmail.com>
2022-03-05 16:19:22 +0600

    console debug console

--- fetch.js
+++ fetch.js
@@ -1,18 +1,15 @@
 console.log(1111111)
 console.log(2222222)
 console.log(3333333)
-console.log(4444444)
 setTimeout(() => {
-   console.log('aaaaaaa') 
-}, 5000);
-console.log(5555555)
-console.log(6666666)
+    console.log('aaaaaaa') 
+ }, 5000);
 fetch('https://jsonplaceholder.typicode.com/todos/1')
-.then(response => response.json())
-.then(json => console.log(json))
-console.log(7777777)
+    .then(response => response.json())
+    .then(json => console.log(json));
+console.log(7777777);
 console.log(1111111);
-for (let i = 0; i < 40; i++) {
+for (let i = 0; i < 5; i++) {
     console.log(i);
     
 }
