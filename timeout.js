9f1cf96d48d23fa053dd2e76d884e2cd634690d3
tanim-k <tanimkhan877@gmail.com>
2022-03-05 13:53:19 +0600

    synchronize

---
+++ timeout.js
@@ -0,0 +1,9 @@
+function doAThing(){
+    console.log('synchronize')
+}
+console.log('first: this one is first');
+console.log('second: this one is second');
+doAThing();
+console.log('third: this one is third');
+console.log('fourth: this one is fourth');
+console.log('fifth: this one is fifth');
