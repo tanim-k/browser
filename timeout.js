09c6e168732c31c8fa273ebb14be5557f2309a7c
tanim-k <tanimkhan877@gmail.com>
2022-03-05 14:11:05 +0600

    synchronize asynchronize

--- timeout.js
+++ timeout.js
@@ -3,7 +3,16 @@ function doAThing(){
 }
 console.log('first: this one is first');
 console.log('second: this one is second');
-doAThing();
+// doAThing();
+// 1::: set time out 
+setTimeout(doAThing, 6000);
+// 2::: direct fn 
+setTimeout(function(){
+    console.log('hello How are you')
+}, 3000)
+setTimeout(() => {
+    console.log('exploring mdn articles')
+}, 4000);
 console.log('third: this one is third');
 console.log('fourth: this one is fourth');
 console.log('fifth: this one is fifth');
